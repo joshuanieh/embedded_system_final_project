8a1aab31c553d5f8f413090f9bdbd203d126cc8e
joshuanieh <b07502027@ntu.edu.tw>
2022-05-04 20:52:04 +0800

    gitignore

--- myconfig.py
+++ myconfig.py
@@ -42,14 +42,14 @@
 # 
 # #STEERING
 # STEERING_CHANNEL = 0            #channel on the 9685 pwm board 0-15
-# STEERING_LEFT_PWM = 230         #pwm value for full left steering
-# STEERING_RIGHT_PWM = 440        #pwm value for full right steering
+# STEERING_LEFT_PWM = 100         #pwm value for full left steering
+# STEERING_RIGHT_PWM = 550        #pwm value for full right steering
 # 
 # #THROTTLE
 # THROTTLE_CHANNEL = 0            #channel on the 9685 pwm board 0-15
 # THROTTLE_FORWARD_PWM = 4095     #pwm value for max forward throttle
 # THROTTLE_STOPPED_PWM = 0        #pwm value for no movement
-# THROTTLE_REVERSE_PWM = -4095    #pwm value for max reverse throttle
+# THROTTLE_REVERSE_PWM = 4095    #pwm value for max reverse throttle
 # 
 # #DC_STEER_THROTTLE with one motor as steering, one as drive
 # #these GPIO pinouts are only used for the DRIVE_TRAIN_TYPE=DC_STEER_THROTTLE

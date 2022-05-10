5b21d79ca05770af186a5352b653d54c248f22d3
joshuanieh <b07502027@ntu.edu.tw>
2022-05-10 23:32:56 +0800

    server1

--- server1.py
+++ server1.py
@@ -1,10 +1,13 @@
 import socket
 import os
 from _thread import *
+import json
 ServerSideSocket = socket.socket()
 host = '192.168.50.226'
 port = 6531
 ThreadCount = 0
+angle = 0.0
+throttle = 0.0
 try:
     ServerSideSocket.bind((host, port))
 except socket.error as e:
@@ -12,13 +15,18 @@ except socket.error as e:
 print('Socket is listening..')
 ServerSideSocket.listen(5)
 def multi_threaded_client(connection):
+    global throttle
     connection.send(str.encode('Server is working:'))
     while True:
         data = connection.recv(2048)
-        response = 'Server message: ' + data.decode('utf-8')
+        print(data)
         if not data:
             break
-        connection.sendall(str.encode(response))
+        if data.decode('utf-8') == "request for control":
+            response = {"angle": 0.0, "throttle": 0.01*throttle}
+            connection.sendall(json.dumps(response))
+        if (data.decode('utf-8'))[0:8] == "distance":
+            throttle = int((data.decode('utf-8'))[10:])
     connection.close()
 while True:
     Client, address = ServerSideSocket.accept()

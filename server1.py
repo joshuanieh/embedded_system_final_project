ad2f24da5a892afa2f007d5d6dc742a8689fb402
joshuanieh <b07502027@ntu.edu.tw>
2022-05-20 15:59:23 +0800

    ?

--- server1.py
+++ server1.py
@@ -16,6 +16,7 @@ print('Socket is listening..')
 ServerSideSocket.listen(5)
 def multi_threaded_client(connection):
     global throttle
+    global angle
     while True:
         data = connection.recv(2048)
         req = data.decode('utf-8')
@@ -24,16 +25,22 @@ def multi_threaded_client(connection):
             break
         if req == "request for control":
             print(throttle)
-            response = {"angle": 0.0, "throttle": throttle}
+            response = {"angle": angle, "throttle": throttle}
             connection.sendall(str.encode(json.dumps(response)))
         if req[0:8] == "distance":
             if "distance" in req[8:]:
                 continue
             else:
-                throttle = int((data.decode('utf-8'))[10:])
+                dis_list = req.split(" ")[1:]
+                throttle = int(dis_list[0])
                 throttle = 2**throttle/320
                 if throttle > 0.8:
                     throttle = 0.8
+                angle = 1/(int(dis_list[1]) + 1)
+                if angle > 1:
+                    angle = -1
+                else:
+                    angle *= -1
     connection.close()
 while True:
     Client, address = ServerSideSocket.accept()

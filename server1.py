0f9b855efb18bd79a1c6e03fa72911e3c30f47b3
joshuanieh <b07502027@ntu.edu.tw>
2022-06-10 19:26:13 +0800

    j

--- server1.py
+++ server1.py
@@ -8,6 +8,10 @@ port = 6531
 ThreadCount = 0
 angle = 0.0
 throttle = 0.0
+dis = 100.0
+right_dis = 100.0
+left_dis = 100.0
+
 try:
     ServerSideSocket.bind((host, port))
 except socket.error as e:
@@ -17,6 +21,9 @@ ServerSideSocket.listen(5)
 def multi_threaded_client(connection):
     global throttle
     global angle
+    global dis
+    global right_dis
+    global left_dis
     while True:
         data = connection.recv(2048)
         req = data.decode('utf-8')
@@ -24,23 +31,47 @@ def multi_threaded_client(connection):
         if not data:
             break
         if req == "request for control":
-            print(throttle)
+            print("throttle: ", throttle, ", angle: ", angle)
             response = {"angle": angle, "throttle": throttle}
+            connection.sendall(str.encode(json.dumps(response)))
+        if req == "request for start":
+            print("throttle: ", 0.7, ", angle: ", angle)
+            response = {"angle": angle, "throttle": 0.7}
+            if dis < 50:
+                response = {"angle": angle, "throttle": throttle}
             connection.sendall(str.encode(json.dumps(response)))
         if req[0:8] == "distance":
             if "distance" in req[8:]:
                 continue
             else:
                 dis_list = req.split(" ")[1:]
-                throttle = int(dis_list[0])
-                throttle = 2**throttle/320
-                if throttle > 0.8:
-                    throttle = 0.8
-                angle = 1/(int(dis_list[1]) + 1)
-                if angle > 1:
-                    angle = -1
+                dis = int(dis_list[0])
+                dis_left = int(dis_list[1])
+                dis_right = int(dis_list[2])
+
+                throttle = dis/500
+                if throttle > 0.5:
+                    throttle = 0.5
+                
+                right_angle = dis_right
+                left_angle = dis_left
+                if right_angle == 5:
+                    right_angle = 4.9
+                if left_angle == 5:
+                    left_angle = 4.9
+
+                right_angle = 1/(right_angle - 5)
+                if right_angle < 0 or right_angle > 1:
+                    right_angle = -1
                 else:
-                    angle *= -1
+                    right_angle *= -1
+
+                left_angle = 1/(left_angle - 5)
+                if left_angle < 0 or left_angle > 1:
+                    left_angle = 1
+                else:
+                    left_angle *= 1
+                angle = left_angle + right_angle
     connection.close()
 while True:
     Client, address = ServerSideSocket.accept()
